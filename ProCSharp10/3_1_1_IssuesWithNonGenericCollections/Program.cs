3400f720aa67e5dd3d9a6ddf3f675f18ab4a7374
Ayush-Nice <ayush.gupta@nice.com>
2025-02-15 14:12:01 +0530

    Collections and generics

---
+++ ProCSharp10/3_1_1_IssuesWithNonGenericCollections/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+WorkWithArrayList();
+
+static void SimpleBoxUnboxOperation()
+{
+    int myInt = 25;
+    object boxedInt = myInt; // Box myInt into a reference type
+    int unboxedInt = (int)boxedInt; // Unbox boxedInt back into a value type
+}
+
+static void WorkWithArrayList()
+{
+    // Value types are automatically boxed when
+    // passed to a member requesting an object.
+    ArrayList myInts = new ArrayList();
+    myInts.Add(10);
+    myInts.Add(20);
+    myInts.Add(35);
+    // Unboxing occurs when we cast the object back
+    // to a value type.
+    int i = (int)myInts[0];
+}
---
+++ ProCSharp10/3_1_2_FunWithCollectionInitialization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Drawing;
+
+int[] myArrayOfInts = { 1, 2, 3, 4, 5 };
+List<int> myGenericList = new List<int> { 1, 2, 3, 4, 5 };
+ArrayList myArrayList = new ArrayList { 1, 2, 3, 4, 5 };
+
+List<Point> myPointList = new List<Point> { new Point(1, 2), new Point(3, 4) };
+foreach (Point p in myPointList)
+{
+    Console.WriteLine(p);
+}
